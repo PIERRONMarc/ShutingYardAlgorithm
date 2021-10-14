142f2f87eac473c702c60dd0d073e75e9a653ddd
PIERRON Marc <pierronmarc@outlook.fr>
2021-10-14 13:04:12 +0200

    refactor: ShutingYard algorithm

--- ShuntingYard.py
+++ ShuntingYard.py
@@ -8,33 +8,43 @@ class Tokenizer:
         self._CharIndex = 0
         self._ExpressionLen = 0
 
+    def _IsExpressionNotEmpty(self):
+        return self._CharIndex < self._ExpressionLen
+
+    def _GetExpressionCurrentChar(self):
+        return self._Expression[self._CharIndex]
+
     def _ParseSpace(self):
-        IsSpace = self._Expression[self._CharIndex] == ' '
-        while self._CharIndex < self._ExpressionLen and self._Expression[self._CharIndex] == ' ':
+        IsSpace = self._GetExpressionCurrentChar() == ' '
+        while self._IsExpressionNotEmpty() and self._GetExpressionCurrentChar() == ' ':
             self._CharIndex += 1
 
         return IsSpace
 
     def _ParseNumber(self):
-        IsANumber = self._Expression[self._CharIndex].isnumeric()
+        def _AddDecimals(numberValue):
+            Weight = 0.1
+            self._CharIndex += 1
+            while self._IsExpressionNotEmpty() and self._GetExpressionCurrentChar().isnumeric():
+                numberValue += Weight * int(self._GetExpressionCurrentChar())
+                Weight /= 10
+                self._CharIndex += 1
+            return numberValue
+
+        IsANumber = self._GetExpressionCurrentChar().isnumeric()
 
         NumberValue = 0
-        while self._CharIndex < self._ExpressionLen and self._Expression[self._CharIndex].isnumeric():
+        while self._IsExpressionNotEmpty() and self._GetExpressionCurrentChar().isnumeric():
             NumberValue *= 10
-            NumberValue += int(self._Expression[self._CharIndex])
+            NumberValue += int(self._GetExpressionCurrentChar())
             self._CharIndex += 1
 
         if self._CharIndex >= self._ExpressionLen:
             self._Token.append(('NUM', NumberValue))
             return IsANumber
 
-        if self._Expression[self._CharIndex] == '.':
-            Weight = 0.1
-            self._CharIndex += 1
-            while self._CharIndex < self._ExpressionLen and self._Expression[self._CharIndex].isnumeric():
-                NumberValue += Weight * int(self._Expression[self._CharIndex])
-                Weight /= 10
-                self._CharIndex += 1
+        if self._GetExpressionCurrentChar() == '.':
+            NumberValue = _AddDecimals(NumberValue)    
 
         if IsANumber:
             self._Token.append(('NUM', NumberValue))
@@ -46,21 +56,31 @@ class Tokenizer:
         if self._CharIndex >= self._ExpressionLen:
             return
 
-        if self._Expression[self._CharIndex] == '(':
+        if self._GetExpressionCurrentChar() == '(':
             self._Token.append(('PARENT', 'OPEN'))
-        elif self._Expression[self._CharIndex] == ')':
+        elif self._GetExpressionCurrentChar() == ')':
             self._Token.append(('PARENT', 'CLOSE'))
         else:
             SwitchOperator = {'+': 'ADD', '-': 'SUB', '*': 'MUL', '/': 'DIV'}
-            self._Token.append(('OP', SwitchOperator.get(self._Expression[self._CharIndex])))
+            self._Token.append(('OP', SwitchOperator.get(self._GetExpressionCurrentChar())))
+
+        self._CharIndex += 1 
 
     def Tokenize(self, Expression):
+        """Tokenize an infix expression
+
+        Args:
+            Expression (string): an infix expression
+
+        Returns:
+            string: a token
+        """
         self._Token = []
         self._Expression = Expression
         self._CharIndex = 0
         self._ExpressionLen = len(Expression)
 
-        while self._CharIndex < self._ExpressionLen:
+        while self._IsExpressionNotEmpty():
             if self._ParseSpace():
                 continue
 
@@ -68,8 +88,6 @@ class Tokenizer:
                 continue
 
             self._ParseOperator()
-
-            self._CharIndex += 1
 
         return self._Token
 
@@ -126,32 +144,67 @@ class Infix2RPN:
         self._RPNCalculator = RPNCalculator()
         self._OperatorStack = []
         self._RPNExpression = []
-
+    
     def Convert2RPN(self, InfixExpression):
-        OperatorPriority = {'ADD': 2, 'SUB': 2, 'MUL': 3, 'DIV': 3}
-        OperatorAssociativity = {'ADD': 'LEFT', 'SUB': 'LEFT', 'MUL': 'LEFT', 'DIV': 'LEFT'}
+        """Convert an infix expression in Reverse Polish Notation
 
+        Args:
+            InfixExpression (string): Infix expression
+
+        Returns:
+            [string]: RPN expression
+        """
         InfixToken = self._Tokenizer.Tokenize(InfixExpression)
 
         for CurrentTokenType, CurrentTokenValue in InfixToken:
             if CurrentTokenType == 'NUM':
-                self._RPNExpression.append(('NUM', CurrentTokenValue))
+                self._HandleNum(CurrentTokenValue);
             elif CurrentTokenValue == 'OPEN':
-                self._OperatorStack.append(('PARENT', 'OPEN'))
+                self._HandleOpen()
             elif CurrentTokenValue == 'CLOSE':
-                self._RPNExpression.append(self._OperatorStack.pop())
-                while len(self._OperatorStack) > 0 and self._OperatorStack[-1] != ('PARENT', 'OPEN'):
-                    self._RPNExpression.append(self._OperatorStack.pop())
+                self._HandleClose()
             elif CurrentTokenType == 'OP':
-                StackTop = len(self._OperatorStack) - 1
-                while StackTop >= 0 and self._OperatorStack[StackTop][0] == 'OP':
-                    if OperatorPriority.get(self._OperatorStack[StackTop][1]) >= OperatorPriority.get(
-                            CurrentTokenValue):
-                        self._RPNExpression.append(self._OperatorStack.pop())
-                    StackTop -= 1
-                self._OperatorStack.append(('OP', CurrentTokenValue))
-
+                self._HandleOperator(CurrentTokenValue)
+                
         return self._RPNExpression + self._OperatorStack
+
+    def _HandleNum(self, value):
+        """Append number to RPN Expression
+
+        Args:
+            value (number): the number
+        """
+        self._RPNExpression.append(('NUM', value))
+
+    def _HandleOpen(self):
+        """Handle open parenthesis
+        """
+        self._OperatorStack.append(('PARENT', 'OPEN'))
+
+    def _HandleClose(self):
+        """Handle closing parenthesis
+        """
+
+        self._RPNExpression.append(self._OperatorStack.pop())
+        while len(self._OperatorStack) > 0 and self._OperatorStack[-1] != ('PARENT', 'OPEN'):
+            self._RPNExpression.append(self._OperatorStack.pop())
+
+    def _HandleOperator(self, value):
+        """Append an operator to the right stack
+
+        Args:
+            value (string): the operator
+        """
+        OperatorPriority = {'ADD': 2, 'SUB': 2, 'MUL': 3, 'DIV': 3}
+
+        StackTop = len(self._OperatorStack) - 1
+        while StackTop >= 0 and self._OperatorStack[StackTop][0] == 'OP':
+            if OperatorPriority.get(self._OperatorStack[StackTop][1]) >= OperatorPriority.get(
+                    value):
+                self._RPNExpression.append(self._OperatorStack.pop())
+            StackTop -= 1
+        self._OperatorStack.append(('OP', value))
+
 
 
 MyRPN = RPNCalculator()
